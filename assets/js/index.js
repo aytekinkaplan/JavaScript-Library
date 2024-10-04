208222bd619ed22254c8ca21094a192c1e632945
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-10-04 08:13:59 +0300

    Up

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,6 @@
 import index from "../css/index.css";
+
+import Excersise1 from "./exercises/Exercise1";
+import Excersise2 from "./exercises/Exercise2";
+import Excersise3 from "./exercises/Exercise3";
+import Excersise4 from "./exercises/Exercise4";
